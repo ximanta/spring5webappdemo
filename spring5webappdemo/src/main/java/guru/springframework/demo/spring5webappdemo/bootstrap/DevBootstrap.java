b308dee9afa47cf7ed390bd691ec419857c150ce
Ximanta <ximanta.sarma@gmail.com>
2017-11-19 20:03:53 +0530

    Added equals and hashCode in models

--- spring5webappdemo/src/main/java/guru/springframework/demo/spring5webappdemo/bootstrap/DevBootstrap.java
+++ spring5webappdemo/src/main/java/guru/springframework/demo/spring5webappdemo/bootstrap/DevBootstrap.java
@@ -46,24 +46,28 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 	private void initData() {
 		//add a few authors and books
 		//Mark Twain
-		Author markTwain = new Author("Mark", "Twain");
+
 		Publisher arthur = new Publisher("Editura Arthur", "C.P. 4, O.P. 83, cod 062610, sector 6, Bucureşti\r\n"); // Saving the phone no. here in case I need it in further app development so I don't have to re-google it :D "Tel: 021.369.31.99"
-		Book tomSawyer = new Book("The Adventures of Tom Sawyer", "9788804305309", arthur);
-		markTwain.getBooks().add(tomSawyer);
-		
 		this.publisherRepo.save(arthur);
-		this.bookRepo.save(tomSawyer);
+
+		Author markTwain = new Author("Mark", "Twain");
+		Book tomSawyer = new Book("The Adventures of Tom Sawyer", "9788804305309", arthur);
+
+        markTwain.getBooks().add(tomSawyer);
+        tomSawyer.getAuthors().add(markTwain);
 		this.authorRepo.save(markTwain);
-		
-		//George Calinescu
-		Author gCalinescu = new Author("George", "Călinescu");
+        this.bookRepo.save(tomSawyer);
+	//George Calinescu
+
 		Publisher litera = new Publisher("Litera International", "Contact. Grup Editorial Litera SRL. B-dul Dimitrie Pompeiu, nr. 5-7, etaj 8, biroul nr. 3, sector 2, Bucureşti, România");
+        this.publisherRepo.save(litera);
+
+        Author gCalinescu = new Author("George", "Călinescu");
 		Book roLitHistory = new Book("Istoria literaturii române de la origini până în prezent", "9789975904704", litera);
-		gCalinescu.getBooks().add(roLitHistory);
-		
-		this.publisherRepo.save(litera);
-		this.bookRepo.save(roLitHistory);
-		this.authorRepo.save(gCalinescu);
+        gCalinescu.getBooks().add(roLitHistory);
+		roLitHistory.getAuthors().add(gCalinescu);
+       	this.authorRepo.save(gCalinescu);
+        this.bookRepo.save(roLitHistory);
 	}
 
 }
--- spring5webappdemo/src/main/java/guru/springframework/demo/spring5webappdemo/model/Book.java
+++ spring5webappdemo/src/main/java/guru/springframework/demo/spring5webappdemo/model/Book.java
@@ -53,4 +53,29 @@ public @Data class Book {
 		this.isbn = isbn;
 		this.publisher = publisher;
 	}
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Book book = (Book) o;
+
+		return id != null ? id.equals(book.id) : book.id == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Book{" +
+				"id=" + id +
+				", title='" + title + '\'' +
+				", isbn='" + isbn + '\'' +
+				", publisher='" + publisher + '\'' +
+				", authors=" + authors +
+				'}';
+	}
 }
